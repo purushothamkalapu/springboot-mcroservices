573de6868e9fe6a9a2861bd1b844d976e36c5dc9
Sri Purushothama <ksripurushotham@yahoo.com>
2023-10-17 13:01:08 +0530

    Initial commit for Service-registory

---
+++ service-registrory/src/main/java/com/purushotham/serviceregistrory/ServiceRegistroryApplication.java
@@ -0,0 +1,13 @@
+package com.purushotham.serviceregistrory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistroryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistroryApplication.class, args);
+	}
+
+}
---
+++ service-registrory/src/test/java/com/purushotham/serviceregistrory/ServiceRegistroryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purushotham.serviceregistrory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistroryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
