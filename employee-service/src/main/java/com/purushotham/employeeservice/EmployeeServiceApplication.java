936d23cbf39fc5c3743a996361e9a65a5b7e4a7b
Sri Purushothama <ksripurushotham@yahoo.com>
2023-10-17 18:03:39 +0530

    Section 14: Service Registry and Discovery using Spring Cloud Netflix Eureka Need to Work Load Balance

--- employee-service/src/main/java/com/purushotham/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/purushotham/employeeservice/EmployeeServiceApplication.java
@@ -3,6 +3,7 @@ package com.purushotham.employeeservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class EmployeeServiceApplication {
     @Bean
     public ModelMapper modelMapper() {
--- employee-service/src/main/java/com/purushotham/employeeservice/service/APIClient.java
+++ employee-service/src/main/java/com/purushotham/employeeservice/service/APIClient.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+//Loadbalancer mutiple instances DepartmentService
+//@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
 
     @GetMapping("api/departments/departmentCode/{departmentCode}")
