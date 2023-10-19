58000d6e15656f23a9ade44792e28f93f00934e7
Purushotham <119475738+purushothamkalapu@users.noreply.github.com>
2023-10-19 15:46:31 +0530

    Initial commit for Config server

---
+++ config-server/src/test/java/com/purushotham/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purushotham.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
